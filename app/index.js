50fd142bfceb4ea23d85c66b96313194c9d5b2c2
marcestruch__ <marcstruchmalonda@gmail.com>
2025-06-11 15:35:38 +0200

    Añadiendo registros y login funcionales  17:00

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+//fix para __dirname
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+//Server
+const app = express();
+app.set("port", 4000);
+app.listen(app.get("port"));
+console.log("Servidor corriendo en puerto", app.get("port"));
+
+//Configuracion
+app.use(express.static(__dirname + "/public"));
+
+//Rutas
+app.get("/", (reg,res)=> res.sendFile( __dirname + "/pages/login.html"));
+app.get("/register", (reg,res)=> res.sendFile( __dirname + "/pages/register.html"));
