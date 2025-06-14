c275f5643d25bfa300c0214184e47cf13d4c85a7
marcestruch__ <marcstruchmalonda@gmail.com>
2025-06-14 14:28:41 +0200

    Implementar funciones de registro y validación de usuarios en el controlador de autenticación

--- app/controllers/authentication.controller.js
+++ app/controllers/authentication.controller.js
@@ -1,9 +1,35 @@
+import bcryptjs from "bcryptjs";
+
+const usuarios =[{
+    user: "a",
+    email: "a@a.com",
+    password: "a"
+}]
+
 async function login(req,res) {
     
 }
 
 async function register(req,res) {
     console.log(req.body);
+    const user = req.body.user;
+    const email = req.body.email;
+    const password = req.body.password;
+    if (!user || !password || !email) {
+        res.status(400).send({status:"Error",message:"Los campos están incompletos"})
+    }
+    const usuarioARevisar = usuarios.find(usuario => usuario.user === user);
+    if (usuarioARevisar) {
+        res.status(400).send({status:"Error",message:"Este usuario ya existe"})
+    }
+    const salt = await bcryptjs.genSalt(5);
+    const hashPassword = await bcryptjs.hash(password,salt);
+    const nuevoUsuario = {
+        user, email, password: hashPassword
+    }
+    console.log(nuevoUsuario),
+    usuarios.push(nuevoUsuario);
+    res.status(201).send({status:"ok",message:`Usuario ${nuevoUsuario.user} agregado`,redirect:"/"})
 }
 
 export const methods = {
