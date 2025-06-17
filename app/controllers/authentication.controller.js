b7e416ee9e9118fbe90b9478aca771319dec9ded
marcestruch__ <marcstruchmalonda@gmail.com>
2025-06-17 14:59:58 +0200

    Añadir autenticación de usuarios con JWT, implementar middleware de autorización y mejorar la gestión de errores en formularios de login y registro.

--- app/controllers/authentication.controller.js
+++ app/controllers/authentication.controller.js
@@ -1,4 +1,8 @@
 import bcryptjs from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const usuarios =[{
     user: "a",
@@ -7,7 +11,31 @@ const usuarios =[{
 }]
 
 async function login(req,res) {
-    
+    console.log(req.body);
+  const user = req.body.user;
+  const password = req.body.password;
+  if(!user || !password){
+    return res.status(400).send({status:"Error",message:"Los campos están incompletos"})
+  }
+  const usuarioAResvisar = usuarios.find(usuario => usuario.user === user);
+  if(!usuarioAResvisar){
+    return res.status(400).send({status:"Error",message:"Error durante login"})
+  }
+  const loginCorrecto = await bcryptjs.compare(password,usuarioAResvisar.password);
+  if(!loginCorrecto){
+    return res.status(400).send({status:"Error",message:"Error durante login"})
+  }
+  const token = jsonwebtoken.sign(
+    {user:usuarioAResvisar.user},
+    process.env.JWT_SECRET,
+    {expiresIn:process.env.JWT_EXPIRATION});
+
+    const cookieOption = {
+      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+      path: "/"
+    }
+    res.cookie("jwt",token,cookieOption);
+    res.send({status:"ok",message:"Usuario loggeado",redirect:"/admin"});
 }
 
 async function register(req,res) {
--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from 'cookie-parser';
 
 //fix para __dirname
 import path from 'path';
 import {fileURLToPath} from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import { methods as authentication } from "./controllers/authentication.controller.js";
+import {methods as authorization} from "./middlewares/authorization.js";
 
 //Server
 const app = express();
@@ -15,11 +17,11 @@ console.log("Servidor corriendo en puerto", app.get("port"));
 //Configuracion
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-
+app.use(cookieParser());
 
 //Rutas
-app.get("/", (req,res)=> res.sendFile( __dirname + "/pages/login.html"));
-app.get("/register", (req,res)=> res.sendFile( __dirname + "/pages/register.html"));
-app.get("/admin", (req,res)=> res.sendFile( __dirname + "/pages/admin/menu.html"));
-app.post("/api/login", authentication.login);
-app.post("/api/register", authentication.register);
+app.get("/",authorization.soloPublico, (req,res)=> res.sendFile(__dirname + "/pages/login.html"));
+app.get("/register",authorization.soloPublico,(req,res)=> res.sendFile(__dirname + "/pages/register.html"));
+app.get("/admin",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/admin/admin.html"));
+app.post("/api/login",authentication.login);
+app.post("/api/register",authentication.register);
